e52fe646c3003da42b6cd43cccf93d5aaa98445a
Neil Lakin <neil.lakin@gmail.com>
2016-03-23 18:07:55 -0400

    try again

--- configure_slave.py
+++ configure_slave.py
@@ -112,7 +112,7 @@ if __name__=="__main__":
     try:
         initGPIOs()
         setSlave(1)
-        # log = open("logfile", 'w')
+        log = open("logfile", 'w')
         out = run('stty -F /dev/ttyMFD1 115200 -parenb -parodd cs8 hupcl -cstopb cread clocal -crtscts -ignbrk -brkint -ignpar -parmrk -inpck -istrip -inlcr -igncr -icrnl -ixon -ixoff -iuclc -ixany -imaxbel iutf8 opost -olcuc -ocrnl onlcr -onocr -onlret -ofill -ofdel nl0 cr0 tab0 bs0 vt0 ff0 -isig -icanon -iexten -echo -echoe -echok -echonl -noflsh -xcase -tostop -echoprt -echoctl -echoke')
         child = connect()
         # child.logfile=log
