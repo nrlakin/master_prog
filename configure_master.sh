af95aa06241ccd9d599a0b3a6f2ad2823f8cc88d
Neil Lakin <neil.lakin@gmail.com>
2016-03-25 16:27:31 -0400

    fix typo in repo url

--- configure_master.sh
+++ configure_master.sh
@@ -1,7 +1,7 @@
 echo "Upgrading opkg repositories...\n"
 echo src/gz all http://repo.opkg.net/edison/repo/all > /etc/opkg/base-feeds.conf
 echo src/gz edison http://repo.opkg.net/edison/repo/edison >> /etc/opkg/base-feeds.conf
-echo src/gz core2-32 http://repo.opgk.net/edison/repo/core2-32 >> /etc/opkg/base-feeds.conf
+echo src/gz core2-32 http://repo.opkg.net/edison/repo/core2-32 >> /etc/opkg/base-feeds.conf
 opkg update
 echo "Upgrading existing installs...\n"
 opkg upgrade
